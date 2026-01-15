b190a5801a5cf5a9abc97b589fe365ff804370a9
Ex333 <haldas.mateusz@gmail.com>
2026-01-15 21:40:51 +0100

    Added ProjectAdmin in admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "is_published",
+        "order",
+        "created_at",
+    )
+
+    list_filter = (
+        "is_published",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "short_description",
+        "description",
+    )
+
+    prepopulated_fields = {
+        "slug": ("title",)
+    }
+
+    list_editable = (
+        "is_published",
+        "order",
+    )
+
+    ordering = (
+        "order",
+        "-created_at",
+    )
+
+    fieldsets = (
+        ("Podstawowe informacje", {
+            "fields": (
+                "title",
+                "slug",
+                "short_description",
+                "description",
+                "image",
+            )
+        }),
+        ("Linki", {
+            "fields": (
+                "url",
+            )
+        }),
+        ("Publikacja", {
+            "fields": (
+                "is_published",
+                "order",
+            )
+        }),
+        ("Daty", {
+            "fields": (
+                "created_at",
+                "updated_at",
+            )
+        }),
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )

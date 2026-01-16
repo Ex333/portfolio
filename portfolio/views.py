5ab8a1d801e5a6ebbeef1e570020d1f6620fe465
Ex333 <haldas.mateusz@gmail.com>
2026-01-16 17:32:08 +0100

    new style.css and a little modifications of htmls - all muse be dynamic

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import Project
 
 def home(request):
     return render(request, 'home.html')
 
+
 def projects(request):
-    return render(request, 'projects.html')
+    projects = Project.objects.filter(is_published=True)
+    return render(request, "projects.html", {
+        "projects": projects
+    })
 
 def about(request):
     return render(request, 'about.html')

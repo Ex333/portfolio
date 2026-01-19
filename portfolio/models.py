fc74d8ee1ac4c334f5973bc20df8fd4ce25c5929
Ex333 <haldas.mateusz@gmail.com>
2026-01-19 06:41:36 +0100

    New model named Skill

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.text import slugify
+
 
 
 class Project(models.Model):
@@ -35,3 +35,24 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
+from django.db import models
+
+
+class Skill(models.Model):
+    CATEGORY_CHOICES = [
+      ("frontend", "Frontend"),
+      ("backend", "Backend"),
+      ("devops", "DevOps"),
+      ("tools", "Tools"),
+      ("other", "Other"),
+  ]
+    name = models.CharField(max_length=64, unique=True,blank=False)
+    category = models.CharField(max_length=40, choices=CATEGORY_CHOICES,default="other")
+    description = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="skills/", blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+        
+
